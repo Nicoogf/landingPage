22a90c9d8716f2dc35f8d47cc99ef94636c00749
Nicolas Falabella <103010434+Nicoogf@users.noreply.github.com>
2023-08-17 15:46:49 -0300

    Se agrega funcionalidad al botton del menu hamburgesa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,45 @@
-import { RiCheckboxBlankCircleFill } from "react-icons/ri" ;
+
+import { useState } from "react";
 
 //Icons
+import { 
+    RiCheckboxBlankCircleFill, 
+    RiMenu3Fill 
+} from "react-icons/ri" ;
 
+function Header() {
+    const[showMenu,setShowMenu] = useState(true); 
 
-function header() {
   return (
-   <header className="flex items-center w-full p-4">
+   <header className="flex items-center justify-between xl:justify-start w-full p-4 h-[10vh] z-50">
 
-    <div className="w-1/6t text-center">
+    <div className="xl:w-1/6 text-center">
 
-        <span className="text-2xl font-bold relative p-1 bg-white">
-            Power <span className="text-primary text-5xl"> .</span>{" "} 
-            <RiCheckboxBlankCircleFill className="absolute -left-3 -bottom-3 text-primary -z-10" /> 
-        </span>
+        <a href="#">
+            <span className="text-2xl font-bold relative p-1 bg-white">
+                Power <span className="text-primary text-5xl"> .</span>{" "} 
+                <RiCheckboxBlankCircleFill className="absolute -left-3 -bottom-3 text-primary -z-10" /> 
+            </span>
+        </a>
+
+        
 
     </div>
 
-    <nav className="flex-1 text-center">
-        <h1>Nav</h1>
+    <nav className={`fixed bg-white w-[80%] xl:w-full h-full 
+    ${ showMenu ? "left-0" : "-left-full" }
+    xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500 top-10 `}>  
+
+        
+        <a href="#" className=""> Home </a>
+        <a href="#" className=""> About </a>
+        <a href="#" className=""> Services </a>
+        <a href="#" className=""> Products </a>
     </nav>
+
+    <button className="text-2xl p-2" onClick={()=> setShowMenu(!showMenu)}>
+         <RiMenu3Fill />
+    </button>
 
 
 
@@ -26,4 +47,4 @@ function header() {
 )
 }
 
-export default header
+export default Header ;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+
+
+function Hero() {
+  return (
+    <section className='bg-red-200 h-[90vh]'>
+        Buenas
+    </section>
+  )
+}
+
+export default Hero ;
